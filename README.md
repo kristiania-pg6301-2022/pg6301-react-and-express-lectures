ae8794961191da66a3a3551d06cff7ce37279c92
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-12 22:36:49 +0100

    List existing movies

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Whirlwind tour of the course
 
-* [ ] Say hello to React
+* [x] Say hello to React
   * `npm init` - creates package.json which contains run scripts and dependencies
   * `npm install --save-dev parcel` add Parcel as a dependency
   * Parcel lets us transform .jsx-files to JavaScript files. It also supports
@@ -10,8 +10,8 @@
   * Add a `start` script: `parcel index.html`
   * Create index.html as our application starting point
   * Move content to index.jsx as a React application
-* [ ] Routing requests in the app
-* [ ] List existing movies
+* [x] Routing requests in the app
+* [x] List existing movies
 * [ ] Add new movies using a form
 * [ ] Encapsulate movies state
 * [ ] Serve React app from Express
--- index.jsx
+++ index.jsx
@@ -3,6 +3,20 @@ import * as ReactDOM from "react-dom";
 
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
+const movies = [
+    {
+        title: "The Matrix",
+        plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        year: 1999
+    },
+    {
+        title: "The Color Purple",
+        plot: "A black Southern woman (Whoopi Goldberg) struggles to find her identity after suffering years of abuse from her father and others over 40 years.",
+        year: 1985
+    }
+]
+
+
 function FrontPage() {
     return <div>
         <h1>Kristiania Movie Database</h1>
@@ -13,12 +27,22 @@ function FrontPage() {
     </div>;
 }
 
+function ListMovies() {
+    return <div>
+        <h1>List movies</h1>
+        {movies.map(m =>
+            <div key={m.title}><h2>{m.title} ({m.year})</h2>
+                <div>{m.plot}</div>
+            </div>)}
+    </div>;
+}
+
 function Application() {
     return <BrowserRouter>
         <Routes>
-            <Route path={"/"} element={<FrontPage />} />
-            <Route path={"/movies"} element={<h1>List movies</h1>} />
-            <Route path={"/movies/new"} element={<h1>Add movie</h1>} />
+            <Route path={"/"} element={<FrontPage/>}/>
+            <Route path={"/movies"} element={<ListMovies/>}/>
+            <Route path={"/movies/new"} element={<h1>Add movie</h1>}/>
         </Routes>
     </BrowserRouter>;
 }
