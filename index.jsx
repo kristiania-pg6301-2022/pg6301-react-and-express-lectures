d854365e472d04ce02f5e9856f3b8ff43537f95a
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-12 22:31:10 +0100

    Add React Router navigation

--- index.jsx
+++ index.jsx
@@ -1,4 +1,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Kristiania Movie Database</h1>, document.getElementById("app"));
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+
+function FrontPage() {
+    return <div>
+        <h1>Kristiania Movie Database</h1>
+        <ul>
+            <li><Link to={"/movies"}>List Movies</Link></li>
+            <li><Link to={"/movies/new"}>Add Movie</Link></li>
+        </ul>
+    </div>;
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<FrontPage />} />
+            <Route path={"/movies"} element={<h1>List movies</h1>} />
+            <Route path={"/movies/new"} element={<h1>Add movie</h1>} />
+        </Routes>
+    </BrowserRouter>;
+}
+
+ReactDOM.render(<Application/>, document.getElementById("app"));
