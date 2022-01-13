87e93bb855e3901be3d4c6afe53d94fa8e1a177a
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-13 15:57:43 +0100

    Navigate around in the app with React Router

--- index.jsx
+++ index.jsx
@@ -1,7 +1,29 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
+
+
+function FrontPage() {
+    return <div>
+        <h1>Kristiania Movie Database</h1>
+        <ul>
+            <li><Link to="/movies">List movies</Link></li>
+            <li><Link to="/movies/new">New Movie</Link></li>
+        </ul>
+    </div>;
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<FrontPage/>}/>
+            <Route path="/movies/new" element={<h1>New movie</h1>}/>
+            <Route path="/movies" element={<h1>List movies</h1>}/>
+        </Routes>
+    </BrowserRouter>;
+}
 
 ReactDOM.render(
-    <h1>Kristiania Movie Database</h1>,
+    <Application/>,
     document.getElementById("app")
 );
