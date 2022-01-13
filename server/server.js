9385a67f34109563cf68e0b062f2765f667177af
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-13 16:40:36 +0100

    Server React HTML built by Parcel

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
+const MOVIES = [
+    {
+        title: "The Matrix",
+        plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        year: 1999
+    },
+    {
+        title: "The Color Purple",
+        plot: "A black Southern woman (Whoopi Goldberg) struggles to find her identity after suffering years of abuse from her father and others over 40 years.",
+        year: 1985
+    }
+];
+
+
 app.get("/api/movies", (req, res) => {
-    res.json([
-        {title: "Hello"}
-    ])
-})
+    res.json(MOVIES);
+});
+
+app.use(express.static(path.resolve("../dist")));
 
 
 const server = app.listen(3000, () => {
