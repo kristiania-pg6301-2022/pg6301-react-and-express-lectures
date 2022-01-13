fcf336ee26206333227c61f5f3f4b26cf6e68401
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-13 16:43:44 +0100

    Support BrowserRouter with universal response middleware

--- server/server.js
+++ server/server.js
@@ -22,6 +22,9 @@ app.get("/api/movies", (req, res) => {
 });
 
 app.use(express.static(path.resolve("../dist")));
+app.use((req, res) => {
+    res.sendFile(path.resolve("..", "dist", "index.html"));
+});
 
 
 const server = app.listen(3000, () => {
