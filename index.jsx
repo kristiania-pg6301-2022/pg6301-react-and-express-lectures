74f9c0fd18185a8aa27a186b9024d9312d2fb31f
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-13 16:01:34 +0100

    List movies in <ListMovies> component

--- index.jsx
+++ index.jsx
@@ -3,6 +3,20 @@ import * as ReactDOM from "react-dom";
 import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
 
 
+const movies = [
+    {
+        title: "The Matrix",
+        plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        year: 1999
+    },
+    {
+        title: "The Color Purple",
+        plot: "A black Southern woman (Whoopi Goldberg) struggles to find her identity after suffering years of abuse from her father and others over 40 years.",
+        year: 1985
+    }
+];
+
+
 function FrontPage() {
     return <div>
         <h1>Kristiania Movie Database</h1>
@@ -13,12 +27,24 @@ function FrontPage() {
     </div>;
 }
 
+function ListMovies() {
+    return <div>
+        <h1>List movies</h1>
+        {movies.map(m =>
+            <>
+                <h2>{m.title} ({m.year})</h2>
+                <div>{m.plot}</div>
+            </>
+        )}
+    </div>;
+}
+
 function Application() {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<FrontPage/>}/>
             <Route path="/movies/new" element={<h1>New movie</h1>}/>
-            <Route path="/movies" element={<h1>List movies</h1>}/>
+            <Route path="/movies" element={<ListMovies/>}/>
         </Routes>
     </BrowserRouter>;
 }
