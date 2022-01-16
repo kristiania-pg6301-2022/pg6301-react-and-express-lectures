4c11351d6dc714365b852d4950bea8ad3903f057
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-16 13:54:19 +0100

    initial quiz game

--- index.jsx
+++ index.jsx
@@ -1,4 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById("app"));
+
+function FrontPage() {
+    return <div>
+        <h1>Quiz app</h1>
+        <button>Take a new quiz</button>
+    </div>;
+}
+
+function QuizGame() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<FrontPage />} />
+        </Routes>
+    </BrowserRouter>;
+}
+
+ReactDOM.render(<QuizGame/>, document.getElementById("app"));
