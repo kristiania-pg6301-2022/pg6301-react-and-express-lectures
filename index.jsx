284a4fdd95d5b402e124e47efea5f15d95efee23
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-16 14:01:12 +0100

    <ShowQuestion> (this time for real)

--- index.jsx
+++ index.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {randomQuestion} from "./questions";
 
 
 function FrontPage() {
     return <div>
         <h1>Quiz app</h1>
-        <Link to={"/question"}><button>Take a new quiz</button></Link>
+        <Link to={"/question"}>
+            <button>Take a new quiz</button>
+        </Link>
     </div>;
 }
 
 function ShowQuestion() {
+    const [question] = useState(randomQuestion());
     return <div>
-        <h1>Here is a question</h1>
-    </div>;
+        <h1>{question.question}</h1>
+        {Object.keys(question.answers)
+            .filter(a => question.answers[a])
+            .map(a => <div key={a}>
+            <button>{question.answers[a]}</button>
+        </div>)}
+    < /div>;
 }
 
 function QuizGame() {
     return <BrowserRouter>
         <Routes>
-            <Route path={"/"} element={<FrontPage />} />
-            <Route path={"/question"} element={<ShowQuestion />} />
+            <Route path={"/"} element={<FrontPage/>}/>
+            <Route path={"/question"} element={<ShowQuestion/>}/>
         </Routes>
     </BrowserRouter>;
 }
