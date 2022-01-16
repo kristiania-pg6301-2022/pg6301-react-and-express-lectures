17d8353f904994e82ab85ca3c5398182d7f6cbb1
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-16 13:56:16 +0100

    <ShowQuestion> placeholder

--- index.jsx
+++ index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
 
 function FrontPage() {
     return <div>
         <h1>Quiz app</h1>
-        <button>Take a new quiz</button>
+        <Link to={"/question"}><button>Take a new quiz</button></Link>
+    </div>;
+}
+
+function ShowQuestion() {
+    return <div>
+        <h1>Here is a question</h1>
     </div>;
 }
 
@@ -14,6 +20,7 @@ function QuizGame() {
     return <BrowserRouter>
         <Routes>
             <Route path={"/"} element={<FrontPage />} />
+            <Route path={"/question"} element={<ShowQuestion />} />
         </Routes>
     </BrowserRouter>;
 }
