30433d0fe6c22e51961a608f34b6f3751e2ce41c
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-16 15:22:24 +0100

    Disable babel presets for parcel to avoid interference between tests and dev code

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-    presets: [
-        "@babel/preset-react",
-        ['@babel/preset-env', {targets: {node: 'current'}}]
-    ],
+module.exports = api => {
+    if (api.env("test")) {
+        api.cache.forever();
+        return {
+            presets: [
+                "@babel/preset-react",
+                ["@babel/preset-env", {targets: {node: "current"}}]
+            ]
+        };
+    }
+    api.cache.forever();
+    return {};
 };
