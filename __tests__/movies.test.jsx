6427f3c92471ef39c385f84c8ada2aa902164cf2
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-19 23:03:32 +0100

    Test that movies creates new movie (passes)

--- __tests__/movies.test.jsx
+++ __tests__/movies.test.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import {render} from "react-dom";
 import {ListMovies, NewMovie} from "../application";
 import {Simulate} from "react-dom/test-utils";
+import {MemoryRouter} from "react-router-dom";
 
 describe("Movies", () => {
     it("runs shows movies", () => {
@@ -14,16 +15,17 @@ describe("Movies", () => {
             }
         ]
         const element = document.createElement("div");
-        render(<ListMovies movies={movies} />, element);
+        render(<ListMovies movies={movies}/>, element);
         expect(element.innerHTML).toMatchSnapshot();
     });
 
     it("creates new movie", () => {
         const element = document.createElement("div");
         const onAddMovie = jest.fn();
-        render(<NewMovie onAddMovie={onAddMovie} />, element);
+        render(<MemoryRouter><NewMovie onAddMovie={onAddMovie}/></MemoryRouter>, element);
         expect(element.innerHTML).toMatchSnapshot();
         Simulate.change(element.querySelector("input"), {target: {value: "Some movie"}});
+        Simulate.submit(element.querySelector("form"));
         expect(onAddMovie).toBeCalledWith({
             title: "Some movie", year: "", plot: ""
         })
