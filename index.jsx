fdf66310bcaa43d27214935d7c4bfa0074dcf595
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-20 16:34:36 +0100

    ListMovies with a MovieCard per movie

--- index.jsx
+++ index.jsx
@@ -12,14 +12,45 @@ function FrontPage() {
     </div>;
 }
 
+const MOVIES = [
+    {
+        title: "Don't look up",
+        plot: "Impending disaster, but will politicians act?",
+        year: 2021
+    },
+    {
+        title: "PG6301",
+        plot: "Johannes Codes, everyone is confused",
+        year: 2022
+    },
+]
+
+function MovieCard({movie}) {
+    const {title, plot, year} = movie;
+    return <div>
+        <h2>{title} ({year})</h2>
+        <p>{plot}</p>
+    </div>;
+}
+
+function ListMovies() {
+    return <div>
+        <h1>Movies</h1>
+        {MOVIES.map(movie => <MovieCard key={movie.title} movie={movie}/>)}
+    </div>;
+}
+
 function MovieApplication() {
     return <Routes>
-        <Route path={"/"} element={<h1>Movies</h1>}/>
+        <Route path={"/"} element={<ListMovies/>}/>
         <Route path={"/new"} element={<h1>Create new movie</h1>}/>
+        <Route path={"*"} element={<h1>Movie not found</h1>}/>
     </Routes>
 }
 
 function Application() {
+
+
     return <BrowserRouter>
         <Routes>
             <Route path={"/"} element={<FrontPage/>}/>
