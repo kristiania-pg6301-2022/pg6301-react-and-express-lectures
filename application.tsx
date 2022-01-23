5bab49e8284fd376d9d7b5a58f1c5b8f4cb5601a
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-23 23:30:21 +0100

    npm format

--- application.tsx
+++ application.tsx
@@ -2,26 +2,39 @@ import * as React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function FrontPage() {
-  return <div>
-    <h1>Movie database</h1>
+  return (
+    <div>
+      <h1>Movie database</h1>
 
-    <ul>
-      <li><Link to={"/movies"}>List movies</Link></li>
-      <li><Link to={"/movies/new"}>Add movie</Link></li>
-    </ul>
-  </div>;
+      <ul>
+        <li>
+          <Link to={"/movies"}>List movies</Link>
+        </li>
+        <li>
+          <Link to={"/movies/new"}>Add movie</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 function Movies() {
-  return null;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>All movies</h1>} />
+      <Route path="/new" element={<h1>Add movie</h1>} />
+    </Routes>
+  );
 }
 
 export function Application({ who }: { who: string }) {
-  return <BrowserRouter>
-    <Routes>
-      <Route path={"/"} element={<FrontPage />} />
-      <Route path={"/movies/*"} element={<Movies />} />
-      <Route path={"/*"} element={<h1>Not found</h1>} />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/movies/*"} element={<Movies />} />
+        <Route path={"/*"} element={<h1>Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
