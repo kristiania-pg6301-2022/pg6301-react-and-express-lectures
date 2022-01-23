fc8f266c994a8d2cf48eaba137a9747cd2ba1659
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-23 23:20:41 +0100

    ignore "dist" directory!

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
         "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended"
     ],
+    "ignorePatterns": ["dist"],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaFeatures": {
