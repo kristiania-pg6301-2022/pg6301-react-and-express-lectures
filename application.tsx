3c8670003546f0108e47844258b7bea10c7b03d3
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-23 23:26:14 +0100

    Create movie application

--- application.tsx
+++ application.tsx
@@ -1,5 +1,27 @@
 import * as React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+function FrontPage() {
+  return <div>
+    <h1>Movie database</h1>
+
+    <ul>
+      <li><Link to={"/movies"}>List movies</Link></li>
+      <li><Link to={"/movies/new"}>Add movie</Link></li>
+    </ul>
+  </div>;
+}
+
+function Movies() {
+  return null;
+}
 
 export function Application({ who }: { who: string }) {
-  return <h1>Hello {who}</h1>;
+  return <BrowserRouter>
+    <Routes>
+      <Route path={"/"} element={<FrontPage />} />
+      <Route path={"/movies/*"} element={<Movies />} />
+      <Route path={"/*"} element={<h1>Not found</h1>} />
+    </Routes>
+  </BrowserRouter>;
 }
