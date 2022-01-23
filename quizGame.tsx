b409451b2815d10d54f736824de414d478a5e1c6
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-23 22:30:29 +0100

    setup eslint rules

--- quizGame.tsx
+++ quizGame.tsx
@@ -31,7 +31,7 @@ export function ShowQuestion({
   setCorrectAnswers: Dispatch<SetStateAction<number>>;
   setQuestionsAnswered: Dispatch<SetStateAction<number>>;
 }) {
-  function handleAnswer(answer: any) {
+  function handleAnswer(answer: string) {
     setQuestionsAnswered((q) => q + 1);
     if (isCorrectAnswer(question, answer)) {
       setCorrectAnswers((q) => q + 1);
