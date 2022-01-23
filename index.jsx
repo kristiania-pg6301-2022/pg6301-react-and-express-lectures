fec7ea7cfcb55625c41989b6cc3d697aea10f89d
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-23 23:05:40 +0100

    Convert jsx files to tsx

--- index.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Application } from "./application";
-
-ReactDOM.render(<Application />, document.getElementById("app"));
---
+++ index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Application } from "./application";
+
+ReactDOM.render(<Application who={"React"} />, document.getElementById("app"));
