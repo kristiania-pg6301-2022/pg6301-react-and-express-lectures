477b91fec86f3bb5fd906e2c570a446db37fdbcf
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-27 16:44:33 +0100

    Add prettier

--- application.jsx
+++ application.jsx
@@ -1,34 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export function ListMovies() {
-    return <div><h1>List movies</h1>
-        <ul>
-            <li>Movie 1</li>
-            <li>Movie 2</li>
-        </ul>
-    </div>;
+  return (
+    <div>
+      <h1>List movies</h1>
+      <ul>
+        <li>Movie 1</li>
+        <li>Movie 2</li>
+      </ul>
+    </div>
+  );
 }
 
-export function NewMovieForm({onAddMovie}) {
-    const [title, setTitle] = useState("");
-    const [year, setYear] = useState("");
-    const [plot, setPlot] = useState("");
+export function NewMovieForm({ onAddMovie }) {
+  const [title, setTitle] = useState("");
+  const [year, setYear] = useState("");
+  const [plot, setPlot] = useState("");
 
-    function handleSubmit() {
-        onAddMovie({title, year, plot});
-    }
+  function handleSubmit() {
+    onAddMovie({ title, year, plot });
+  }
 
-    return <form onSubmit={handleSubmit}>
-        <h1>Add new movie</h1>
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Add new movie</h1>
+      <div>
+        Title:{" "}
+        <input
+          data-testid="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        Year:{" "}
+        <input
+          data-testid="year"
+          value={year}
+          onChange={(e) => setYear(e.target.value)}
+        />
+      </div>
+      <div>
+        Plot:
         <div>
-            Title: <input data-testid="title" value={title} onChange={e => setTitle(e.target.value)} />
+          <textarea data-testid="plot" />
         </div>
-        <div>
-            Year: <input data-testid="year" value={year} onChange={e => setYear(e.target.value)} />
-        </div>
-        <div>
-            Plot:
-            <div><textarea data-testid="plot" /></div>
-        </div>
-    </form>;
+      </div>
+    </form>
+  );
 }
--- index.jsx
+++ index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {ListMovies} from "./application";
+import { ListMovies } from "./application";
 
-ReactDOM.render(<ListMovies/>, document.getElementById("app"));
+ReactDOM.render(<ListMovies />, document.getElementById("app"));
