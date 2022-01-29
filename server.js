bb12b9e948e98e67b09b3067c61018bb46083135
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-29 10:23:48 +0100

    respond to POST request

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get("/login", (req, res) => {
   res.json({
@@ -13,6 +15,7 @@ const USERS = [];
 app.post("/users", (req, res) => {
   const {username, fullname, password} = req.body;
   USERS.push({username, fullname, password});
+  res.sendStatus(200)
 })
 
 const server = app.listen(process.env.PORT || 3000, () => {
