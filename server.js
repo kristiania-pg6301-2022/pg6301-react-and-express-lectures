39138a6bf3199a05b965d968b463991c7a6baf5f
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-29 09:36:37 +0100

    `npm install express`, const app = express(), app.listen()

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("Hello Node JS and Nodemon");
+import express from "express";
+
+const app = express();
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on http://localhost:${server.address().port}`);
+})
