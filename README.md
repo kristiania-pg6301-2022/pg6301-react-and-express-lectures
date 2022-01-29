6a7627f2c357b57956be1d1ed9bded47b4a8ae64
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-29 10:22:36 +0100

    respond to POST request (failing)

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 Express - a web server framework for Node JS
 ============================================
 
-* [ ] Responding to simple requests
+* [x] Responding to simple requests
+  * [x] get
+  * [ ] post
 * [ ] JSON
+* [ ] static files
 * [ ] Cookies
 * [ ] Middleware
   * Enforce user set
--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.get("/login", (req, res) => {
   })
 })
 
+const USERS = [];
+
+app.post("/users", (req, res) => {
+  const {username, fullname, password} = req.body;
+  USERS.push({username, fullname, password});
+})
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on http://localhost:${server.address().port}`);
 })
