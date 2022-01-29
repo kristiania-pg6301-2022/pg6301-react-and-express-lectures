48c18a0e803db151fde611743c9746593ace33b7
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-29 10:12:57 +0100

    respond to GET request

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get("/login", (req, res) => {
+  res.json({
+    username: "admin"
+  })
+})
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on http://localhost:${server.address().port}`);
 })
