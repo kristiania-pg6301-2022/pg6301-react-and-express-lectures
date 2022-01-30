ad44774ca1979d9e9269dad9c63625cfb7db7ede
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-30 21:38:42 +0100

    Setup middleware in server.ts

--- server.ts
+++ server.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { AddressInfo } from "net";
 import { QuizApp } from "./quizApp";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use("/quiz", QuizApp);
 
 const server = app.listen(process.env.PORT || 3000, () => {
