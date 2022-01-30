7a64be68f0bb67a5ed3dfb28a6877d8a7ea4c3b7
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-30 20:33:55 +0100

    start express in server.ts

--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
-console.log("Hello ts-node");
+import express from "express";
+import { AddressInfo } from "net";
+
+const app = express();
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `started on http://localhost:${(server.address() as AddressInfo).port}`
+  );
+});
