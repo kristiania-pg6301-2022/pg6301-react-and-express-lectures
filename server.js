6880021e5ea830d3a547ea779efd6a0efe340b27
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-04 14:02:07 +0100

    (sort of) respond to POST

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get("/login", (req, res) => {
   });
 })
 
+app.post("/login", (req, res) => {
+  // set something so that GET /login returns user name
+  console.log("Test");
+  res.end()
+})
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`server started at http://localhost:${server.address().port}`);
 })
