f0082840721fc22f0143ec44143b544a9777f199
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-07 00:02:06 +0100

    Silly (non-working) express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-console.log("Hello Express")
+import express from "express";
+
+const app = express();
+
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Started on http://localhost:${server.address().port}`)
+})
