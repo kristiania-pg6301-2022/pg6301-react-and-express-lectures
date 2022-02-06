ef4d1e5da4fd290be929a9cfc1315db3c0a1f1a4
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-07 00:15:36 +0100

    Extract Login as separate express Router

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import {LoginRouter} from "./loginRouter.js";
 
 dotenv.config();
 
@@ -10,36 +11,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 
-const users = [
-    {
-        username: "admin",
-        password: "321terces",
-        fullName: "Test Persson"
-    }
-];
-
 app.use(express.static("public"));
 
-app.get("/login", (req, res) => {
-    const {username} = req.signedCookies;
-    const user = users.find(u => u.username === username);
-    if (!user) {
-        return res.sendStatus(401);
-    }
-    const {fullName} = user;
-    return res.json({fullName, username});
-})
-
-app.post("/login", (req, res) => {
-    const {username, password} = req.body;
-    const user = users.find(u => u.username === username);
-    if (user && user.password === password) {
-        res.cookie("username", username, {signed: true});
-        return res.sendStatus(200);
-    } else {
-        return res.sendStatus(401);
-    }
-})
+app.use("/login", LoginRouter);
 
 
 const server = app.listen(process.env.PORT || 3000, () => {
