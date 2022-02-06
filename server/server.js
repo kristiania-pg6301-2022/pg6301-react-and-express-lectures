e3d1b8df3ad5745ecff2666abbd6cd5bd72b851e
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-07 00:05:20 +0100

    Static pages

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 
 
+app.use(express.static("public"));
+
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Started on http://localhost:${server.address().port}`)
 })
