1cf908c23542f34808dcc2ac2167fe9d86b0f4f8
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-09 23:10:24 +0100

    serve react with browserrouter from server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 import express from "express";
 import { randomQuestion } from "./questions.js";
+import * as path from "path";
 
 const app = express();
 
 app.get("/question/random", (req, res) => {
   const question = randomQuestion();
   res.json(question);
+});
+
+app.use(express.static(path.resolve("../client/dist")));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
