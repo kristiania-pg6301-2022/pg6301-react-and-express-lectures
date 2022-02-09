fb92d078a2f1bae308b40be48e0db69ea25f0e13
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-09 22:22:10 +0100

    Test cookies and post requests with supertest

--- server/__tests__/login.test.js
+++ server/__tests__/login.test.js
@@ -2,12 +2,32 @@ import request from "supertest";
 
 import express from "express";
 import { LoginRouter } from "../loginRouter";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(cookieParser("test secret"));
+app.use(bodyParser.json());
 app.use("/", LoginRouter);
 
 describe("login router", () => {
   it("shows 401 Unauthorized to non-logged in users", () => {
     request(app).get("/").expect(401);
+  });
+  it("fails login with invalid credentials", () => {
+    request(app)
+      .post("/")
+      .send({ username: "nobody", password: "special" })
+      .expect(401);
+  });
+  it("logs in with correct credentials", async () => {
+    const agent = request.agent(app);
+    await agent
+      .post("/")
+      .send({ username: "admin", password: "321terces" })
+      .expect(200);
+    await agent
+      .get("/")
+      .expect({ username: "admin", fullName: "Test Persson" });
   });
 });
