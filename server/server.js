abc5f897320ae15959b975f99a19c7fd0ecf26b3
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-09 22:40:17 +0100

    Add pre-commit with husky and pretier

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`server started on http://localhost:${server.address().port}`)
-})
+  console.log(`server started on http://localhost:${server.address().port}`);
+});
