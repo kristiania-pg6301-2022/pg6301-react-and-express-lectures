96df30c9c3f456baeb1d06ae67b745b2596bb4b6
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-09 22:32:37 +0100

    npm install express, start (empty) express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+import express from "express";
+
+const app = express();
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`server started on http://localhost:${server.address().port}`)
+})
