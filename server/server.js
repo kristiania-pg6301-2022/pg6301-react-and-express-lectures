7dcb4c86567643c3511fb4af177cc7c138dfeffe
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-09 22:50:47 +0100

    GET /question/random backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 import express from "express";
+import { randomQuestion } from "./questions.js";
 
 const app = express();
+
+app.get("/question/random", (req, res) => {
+  const question = randomQuestion();
+  res.json(question);
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`server started on http://localhost:${server.address().port}`);
