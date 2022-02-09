d8f2a984cacb44fa6da1265761b84308e87ae133
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-09 21:19:40 +0100

    enable prettier with husky (git commit hook)
    * npm install husky prettier
    * npx husky install
    * npm set-script prepare "husky install"
    * npx husky add .husky/pre-commit "npm run format:check"

--- client/index.jsx
+++ client/index.jsx
@@ -1,79 +1,106 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Link, Route, Routes, useNavigate} from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
 function FrontPage() {
-    const [loading, setLoading] = useState(true);
-    const [user, setUser] = useState();
-    useEffect(async () => {
-        setLoading(true);
-        const res = await fetch("/api/login");
-        if (res.ok) {
-            setUser(await res.json());
-        } else if (res.status === 401) {
-            setUser(null);
-        }
-        setLoading(false);
-    }, []);
-
-    if (loading) {
-        return <div>Loading...</div>;
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState();
+  useEffect(async () => {
+    setLoading(true);
+    const res = await fetch("/api/login");
+    if (res.ok) {
+      setUser(await res.json());
+    } else if (res.status === 401) {
+      setUser(null);
     }
+    setLoading(false);
+  }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    return <div>
-        <h1>Front page</h1>
-        {
-            user
-                ? <div>Welcome {user.fullName}</div>
-                : <div><Link to={"/login"}>Login</Link></div>
-        }
-    </div>;
+  return (
+    <div>
+      <h1>Front page</h1>
+      {user ? (
+        <div>Welcome {user.fullName}</div>
+      ) : (
+        <div>
+          <Link to={"/login"}>Login</Link>
+        </div>
+      )}
+    </div>
+  );
 }
 
 function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState();
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState();
+  const navigate = useNavigate();
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        const result = await fetch("/api/login", {
-            method: "POST",
-            body: JSON.stringify({username, password}),
-            headers: {
-                "content-type": "application/json"
-            }
-        });
-        if (result.ok) {
-            navigate("/");
-        } else {
-            setError("Login failed");
-        }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const result = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    if (result.ok) {
+      navigate("/");
+    } else {
+      setError("Login failed");
     }
+  }
 
-    return <div>
-        <h1>Login</h1>
-        <form onSubmit={handleSubmit}>
-            <div>Username: <input name="username" value={username} onChange={e => setUsername(e.target.value)}/></div>
-            <div>Password: <input name="password" type="password" value={password}
-                                  onChange={e => setPassword(e.target.value)}/></div>
-            {error && <div>{error}</div>}
-            <div>
-                <button>Log in</button>
-            </div>
-        </form>
-    </div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Username:{" "}
+          <input
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          Password:{" "}
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {error && <div>{error}</div>}
+        <div>
+          <button>Log in</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 function Application() {
-    return <BrowserRouter>
-        <Routes>
-            <Route path={"/"} element={<FrontPage/>}/>
-            <Route path={"/login"} element={<Login/>}/>
-        </Routes>
-    </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-ReactDOM.render(<Application/>, document.getElementById("app"));
+ReactDOM.render(<Application />, document.getElementById("app"));
