a5c1b96c84b38bbc9688e8bd5b40043f74c23ba7
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-11 14:43:11 +0100

    Integrate React with parcel in Express

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.post("/login", (req, res, next) => {
   res.sendStatus(401);
 });
 
-app.use(express.static("public"));
+app.use(express.static("../client/dist"));
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
