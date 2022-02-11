a2d7012a58883d4b0cbaab25c34da5f10053a5e8
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-11 13:57:12 +0100

    Use port from environment (for Heroku)

--- server/server.js
+++ server/server.js
@@ -9,4 +9,6 @@ app.post("/login", (req, res, next) => {
   res.sendStatus(401);
 })
 
-app.listen(3000);
+const server = app.listen( process.env.PORT || 3000, () => {
+  console.log(`Started on http://localhost:${server.address().port}`);
+});
