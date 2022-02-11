bb94586c2458af9081e5222042e964be19309e06
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-11 14:25:38 +0100

    Add prettier and husky

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ import express from "express";
 const app = express();
 
 app.get("/login", (req, res, next) => {
-  res.json({username: "Noen André Persson"});
-})
+  res.json({ username: "Noen André Persson" });
+});
 app.post("/login", (req, res, next) => {
   res.sendStatus(401);
-})
+});
 
-const server = app.listen( process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
 });
