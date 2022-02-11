0e0d3a227a1bd190559bd281b57587aa220a87f2
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-11 13:07:33 +0100

    Enable React on Heroku

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
@@ -19,7 +19,38 @@ function FrontPage() {
 }
 
 function AnswerQuestion() {
-  return <h1>Show question</h1>;
+  function handleConfirm(answer) {
+    fetch(`/questions/${question.id}/answer`, {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ answer }),
+    });
+  }
+
+  const [question, setQuestion] = useState();
+  useEffect(async () => {
+    const res = await fetch("/question/random");
+    setQuestion(await res.json());
+  }, []);
+
+  if (!question) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>{question.question}</h1>
+      {Object.keys(question.answers)
+        .filter((a) => question.answers[a])
+        .map((answer) => (
+          <div key={answer}>
+            <button onClick={(q) => handleConfirm("sndldn")}>
+              {question.answers[answer]}
+            </button>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 function ShowScore() {
