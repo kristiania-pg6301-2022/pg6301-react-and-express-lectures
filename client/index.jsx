50f4704288111354cc5ad975a72118d67deacb8d
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-16 01:37:15 +0100

    React router on client

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>Movies database</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Movies database</h1>
+
+      <div>
+        <Link to={"/login"}>Log in</Link>
+        <Link to={"/register"}>Sign up</Link>
+      </div>
+    </div>
+  );
+}
+
+function Login() {
+  return <h1>Log in</h1>;
+}
+
+function NewUser() {
+  return <h1>Create new user</h1>;
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/register"} element={<NewUser />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
