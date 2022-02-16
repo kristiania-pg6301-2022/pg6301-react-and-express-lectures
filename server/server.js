cd883a92508b5979eece0bc39f051a3ea63b8f33
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-02-16 01:47:33 +0100

    serve browser router from express

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
 
 app.get("/api/login", (req, res) => {
   return res.json({ username: "Johannes", fullName: "Johannes Brodwall" });
+});
+
+app.use(express.static("../client/dist"));
+app.use((req, res) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 5000, () => {
