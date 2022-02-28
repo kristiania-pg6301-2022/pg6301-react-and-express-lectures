9f03a4bbc635d54947fe9efdccb2a6132161f3a9
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-01 00:35:28 +0100

    Add application pages

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>Movie Database</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Movie Database</h1>
+      <ul>
+        <li>
+          <Link to={"/movies/list"}>List movies</Link>
+        </li>
+        <li>
+          <Link to={"/movies/new"}>Add new movie</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Movies() {
+  return (
+    <Routes>
+      <Route path={"/list"} element={<h1>List movies</h1>} />
+      <Route path={"/new"} element={<h1>Add new movie</h1>} />
+    </Routes>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/movies/*"} element={<Movies />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
