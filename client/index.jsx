eb66f43b5cb7f06fdc52c3ecfce52b2ac450216f
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-01 00:43:21 +0100

    ListMovies along with useLoader and fetchJSON

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
@@ -18,10 +18,66 @@ function FrontPage() {
   );
 }
 
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Failed ${res.status}: ${(await res).statusText}`);
+  }
+  return await res.json();
+}
+
+function useLoader(loadingFunction) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+
+  async function load() {
+    try {
+      setLoading(true);
+      setData(await loadingFunction());
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => load(), []);
+
+  return { loading, error, data };
+}
+
+function ListMovies() {
+  const { loading, error, data } = useLoader(async () => {
+    return fetchJSON("/api/movies");
+  });
+
+  if (loading) {
+    return <div>Loading.... </div>;
+  }
+  if (error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <div>{error.toString()}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>List movies</h1>
+      {data.map((movie) => (
+        <div key={movie.title}>{movie.title}</div>
+      ))}
+    </div>
+  );
+}
+
 function Movies() {
   return (
     <Routes>
-      <Route path={"/list"} element={<h1>List movies</h1>} />
+      <Route path={"/list"} element={<ListMovies />} />
       <Route path={"/new"} element={<h1>Add new movie</h1>} />
     </Routes>
   );
