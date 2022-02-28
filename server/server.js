1492abf22e1ed37b89808f70bc743b6d8df1c80e
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-01 00:27:35 +0100

    React frontend (step 1)

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.static("../client/dist/"));
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`started on http://localhost:${server.address().port}`);
 });
