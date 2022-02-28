2e6f29c3fba0b7e98cfaa03a4b4b16aa320930f9
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-01 00:36:57 +0100

    Add application pages (express support for BrowserRouter)

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
 
 app.use(express.static("../client/dist/"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`started on http://localhost:${server.address().port}`);
