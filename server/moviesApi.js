5cd7d109d872b42f98d496fd4d20103e9f2474a1
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-01 17:17:26 +0100

    Insert movies into MongoDB

--- server/moviesApi.js
+++ server/moviesApi.js
@@ -24,6 +24,7 @@ export function MoviesApi(db) {
   api.post("/", (req, res) => {
     const { title, plot, year } = req.body;
     movies.push({ title, plot, year });
+    db.collection("movies").insertOne({ title, plot, year });
     res.sendStatus(204);
   });
   return api;
