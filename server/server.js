66c1b754b3beb06d331bb0f0a5ae5678b06b426f
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-01 18:48:42 +0100

    Include mongodb in server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 import express from "express";
+import {MongoClient} from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/api/movies", (req, res) => {
-  res.json([
-    { title: "Movie 1", plot: "The first movie" },
-    { title: "Movie 2", plot: "The second movie" },
-  ])
-})
-
+if (process.env.ATLAS_URL) {
+  const client = new MongoClient(process.env.ATLAS_URL);
+  client.connect().then(connection => {
+    app.get("/api/movies", (req, res) => {
+      res.json([
+        { title: "Movie 1", plot: "The first movie" },
+        { title: "Movie 2", plot: "The second movie" },
+      ])
+    });
+  });
+}
 
 app.use(express.static("../client/dist/"));
 
