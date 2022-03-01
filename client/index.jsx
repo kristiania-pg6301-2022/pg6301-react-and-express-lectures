0e102b3305403e14b395dc9a9ee4c8834cce2168
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-01 18:43:55 +0100

    List movies from server

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById("app"));
+function useLoader(loadingFunction) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState(undefined);
+  async function load() {
+    try {
+      setLoading(true);
+      setError(undefined);
+      setData(await loadingFunction());
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+  return {loading, error, data}
+}
+
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`${res.status}: ${res.statusText}`);
+  }
+  return await res.json();
+}
+
+
+function Application() {
+  const {loading, error, data} = useLoader(async () => await fetchJSON("/api/movies"));
+
+  if (loading) {
+    return <div>Loading....</div>;
+  }
+  if (error) {
+    return <div>Error: {error.toString()}</div>
+  }
+
+  return <div>
+    <h1>Movies</h1>
+    {data.map(movie => <div key={movie.title}>{movie.title}</div>)}
+  </div>;
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -2,7 +2,15 @@ import express from "express";
 
 const app = express();
 
-app.use(express.static("../client/dist/"))
+app.get("/api/movies", (req, res) => {
+  res.json([
+    { title: "Movie 1", plot: "The first movie" },
+    { title: "Movie 2", plot: "The second movie" },
+  ])
+})
+
+
+app.use(express.static("../client/dist/"));
 
 const server = app.listen(3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
