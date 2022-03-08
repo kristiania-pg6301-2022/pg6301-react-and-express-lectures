4480bd520e8ea4013f84fd3aa0c99b58b16aebe0
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-08 23:30:20 +0100

    Generate authorizationUrl

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
@@ -11,13 +11,48 @@ function FrontPage() {
   );
 }
 
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error();
+  }
+  return await res.json();
+}
+
+function Login() {
+  const [authorizationUrl, setAuthorizationUrl] = useState();
+  useEffect(async () => {
+    const discoveryDocument = await fetchJSON(
+      "https://accounts.google.com/.well-known/openid-configuration"
+    );
+    const { authorization_endpoint } = discoveryDocument;
+    const params = {
+      client_id:
+        "34816606807-qttvokcc056kt9bje518mt8mr6dnvisg.apps.googleusercontent.com",
+      response_type: "token",
+      response_mode: "fragment",
+      scope: "openid email profile",
+      redirect_uri: window.location.origin + "/login/callback",
+    };
+    const authorizationUrl =
+      authorization_endpoint + "?" + new URLSearchParams(params);
+    setAuthorizationUrl(authorizationUrl);
+  }, []);
+  return (
+    <>
+      <h1>Login</h1>
+      <a href={authorizationUrl}>Login</a>
+    </>
+  );
+}
+
 function Application() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<FrontPage />} />
         <Route path={"/profile"} element={<h1>User profile</h1>} />
-        <Route path={"/login"} element={<h1>Login</h1>} />
+        <Route path={"/login"} element={<Login />} />
         <Route path={"/login/callback"} element={<h1>Login callback</h1>} />
       </Routes>
     </BrowserRouter>
