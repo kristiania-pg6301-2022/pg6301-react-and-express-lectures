ddc170e8f5bed7412f311ff8d4e6c57b1c444589
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-13 22:17:24 +0100

    Layout

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+function Application() {
+  return (
+    <>
+      <header>Kristiania Chat</header>
+      <main>
+        <div>Message 1</div>
+        <div>Message 2</div>
+      </main>
+      <footer>
+        <form>
+          <input autoFocus={true} />
+          <button>Send</button>
+        </form>
+      </footer>
+    </>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
