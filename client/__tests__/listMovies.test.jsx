5fdd34ff08f988744212d90ca6cea41aa2f92432
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-20 22:59:02 +0100

    extract function renderListMovies to promote function reuse

--- client/__tests__/listMovies.test.jsx
+++ client/__tests__/listMovies.test.jsx
@@ -16,57 +16,36 @@ const movies = [
   },
 ];
 
+async function renderListMovies(listMovies) {
+  const element = document.createElement("div");
+  await act(async () =>
+    ReactDOM.render(<ListMovies movieApi={{ listMovies }} />, element)
+  );
+  return element;
+}
+
 describe("list movies", () => {
   it("shows loading screen", async () => {
-    const element = document.createElement("div");
-    await act(async () =>
-      ReactDOM.render(
-        <ListMovies
-          movieApi={{
-            listMovies: () =>
-              new Promise(() => {
-                // do nothing
-              }),
-          }}
-        />,
-        element
-      )
-    );
+    const element = await renderListMovies(() => new Promise(() => {}));
     expect(element.querySelector(".loading-indicator")).not.toBeNull();
     expect(element.innerHTML).toMatchSnapshot();
   });
 
   it("shows movie list", async () => {
-    const element = document.createElement("div");
-    await act(async () => {
-      ReactDOM.render(
-        <ListMovies
-          movieApi={{
-            listMovies: () => new Promise((resolve) => resolve(movies)),
-          }}
-        />,
-        element
-      );
-    });
+    const element = await renderListMovies(
+      () => new Promise((resolve) => resolve(movies))
+    );
     expect(element.querySelector("h3").innerHTML).toEqual(movies[0].title);
     expect(element.innerHTML).toMatchSnapshot();
   });
 
   it("shows error message", async () => {
-    const element = document.createElement("div");
-    await act(async () => {
-      ReactDOM.render(
-        <ListMovies
-          movieApi={{
-            listMovies: () =>
-              new Promise((resolve, reject) => {
-                reject(new Error("Failed to fetch"));
-              }),
-          }}
-        />,
-        element
-      );
-    });
+    const element = await renderListMovies(
+      () =>
+        new Promise((resolve, reject) => {
+          reject(new Error("Failed to fetch"));
+        })
+    );
     expect(element.querySelector(".error-message").innerHTML).toContain(
       "Failed to fetch"
     );
