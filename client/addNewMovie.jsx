daf44fe12d333ee9962f30a0ad7ff9c8c4d4e32e
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-21 00:41:39 +0100

    All properties should be added with useState

--- client/addNewMovie.jsx
+++ client/addNewMovie.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function AddNewMovie({ movieApi }) {
   function handleSubmit() {
     movieApi.createMovie({ title, year, country, plot });
   }
+
+  const [title, setTitle] = useState("");
+  const [year, setYear] = useState("");
+  const [country, setCountry] = useState("");
+  const [plot, setPlot] = useState("");
 
   return (
     <form onSubmit={handleSubmit}>
