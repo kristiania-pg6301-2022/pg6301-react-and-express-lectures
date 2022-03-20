e49580b9a36038d95060d1478b933906c8d02ba2
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-20 22:12:22 +0100

    extract fetchJSON and useLoading to own files

--- client/listMovies.jsx
+++ client/listMovies.jsx
@@ -1,35 +1,6 @@
-import React, { useEffect, useState } from "react";
-
-function useLoading(loadingFunction) {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
-  const [data, setData] = useState();
-
-  async function load() {
-    try {
-      setLoading(true);
-      setData(await loadingFunction());
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    load();
-  }, []);
-
-  return { loading, error, data };
-}
-
-async function fetchJSON(url) {
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error(`Failed to load ${res.status}: ${res.statusText}`);
-  }
-  return await res.json();
-}
+import React from "react";
+import { fetchJSON } from "./fetchJSON";
+import { useLoading } from "./useLoading";
 
 function MovieCard({ movie: { title, plot, poster } }) {
   return (
