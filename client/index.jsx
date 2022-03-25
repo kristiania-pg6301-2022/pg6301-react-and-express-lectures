bbc07d64f5a6f09cc6610d44ad24b38ae6d6e09e
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-25 13:36:30 +0100

    Refactoring to enable testing

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { ListMovies } from "./listMovies";
 
 function FrontPage() {
   return (
@@ -18,73 +19,12 @@ function FrontPage() {
   );
 }
 
-function useLoading(loadingFunction) {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
-  const [data, setData] = useState();
-
-  async function load() {
-    try {
-      setLoading(true);
-      setData(await loadingFunction());
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    load();
-  }, []);
-
-  return { loading, error, data };
-}
-
 async function fetchJSON(url) {
   const res = await fetch(url);
   if (!res.ok) {
     throw new Error(`Failed to load ${res.status}: ${res.statusText}`);
   }
   return await res.json();
-}
-
-function MovieCard({ movie: { title, plot, poster } }) {
-  return (
-    <>
-      <h3>{title}</h3>
-      {poster && <img src={poster} width={100} alt={"Movie poster"} />}
-      <div>{plot}</div>
-    </>
-  );
-}
-
-function ListMovies() {
-  const { loading, error, data } = useLoading(async () =>
-    fetchJSON("/api/movies")
-  );
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return (
-      <div>
-        <h1>Error</h1>
-        <div>{error.toString()}</div>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <h1>Movies in the database</h1>
-
-      {data.map((movie) => (
-        <MovieCard key={movie.title} movie={movie} />
-      ))}
-    </div>
-  );
 }
 
 function AddNewMovie() {
@@ -96,11 +36,17 @@ function AddNewMovie() {
 }
 
 function Application() {
+  async function listMovies() {
+    return await fetchJSON("/api/movies");
+  }
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<FrontPage />} />
-        <Route path={"/movies"} element={<ListMovies />} />
+        <Route
+          path={"/movies"}
+          element={<ListMovies listMovies={listMovies} />}
+        />
         <Route path={"/movies/new"} element={<AddNewMovie />} />
       </Routes>
     </BrowserRouter>
---
+++ client/listMovies.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useLoading } from "./useLoading";
+
+function MovieCard({ movie: { title, plot, poster } }) {
+  return (
+    <>
+      <h3>{title}</h3>
+      {poster && <img src={poster} width={100} alt={"Movie poster"} />}
+      <div>{plot}</div>
+    </>
+  );
+}
+
+export function ListMovies({ listMovies }) {
+  const { loading, error, data } = useLoading(listMovies);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <div>{error.toString()}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Movies in the database</h1>
+
+      {data.map((movie) => (
+        <MovieCard key={movie.title} movie={movie} />
+      ))}
+    </div>
+  );
+}
