9151635c2857b0dd56a29a8fa6241757e8278efa
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-25 13:53:25 +0100

    List Movies test shows movies from the database

--- client/__tests__/listMovies.test.jsx
+++ client/__tests__/listMovies.test.jsx
@@ -2,11 +2,21 @@ import { ListMovies } from "../listMovies";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
 
 describe("ListMovies component", () => {
   it("shows loading screen", () => {
     const domElement = document.createElement("div");
     ReactDOM.render(<ListMovies />, domElement);
+    expect(domElement.innerHTML).toMatchSnapshot();
+  });
+
+  it("shows movies", async () => {
+    const movies = [{ title: "movie 1" }, { title: "movie 2" }];
+    const domElement = document.createElement("div");
+    await act(async () => {
+      ReactDOM.render(<ListMovies listMovies={() => movies} />, domElement);
+    });
     expect(domElement.innerHTML).toMatchSnapshot();
   });
 });
