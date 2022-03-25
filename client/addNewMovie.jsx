6f1fbcf47666ade0487f5dae4732100a8e792346
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-25 15:12:41 +0100

    Add new test for add movie complete

--- client/addNewMovie.jsx
+++ client/addNewMovie.jsx
@@ -13,11 +13,21 @@ function FormInput({ label, value, onChangeValue }) {
 
 export function AddNewMovie() {
   const [title, setTitle] = useState("");
+  const [year, setYear] = useState("");
+  const [country, setCountry] = useState("");
+  const [plot, setPlot] = useState("");
   return (
     <form>
       <h1>Add new movie</h1>
 
       <FormInput label={"Title:"} value={title} onChangeValue={setTitle} />
+      <FormInput label={"Year:"} value={year} onChangeValue={setYear} />
+      <FormInput
+        label={"Country:"}
+        value={country}
+        onChangeValue={setCountry}
+      />
+      <FormInput label={"Plot:"} value={plot} onChangeValue={setPlot} />
     </form>
   );
 }
