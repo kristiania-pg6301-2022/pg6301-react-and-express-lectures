9bd725a9d0634e21e40ce38b4e18dd7f96d64bd3
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-25 15:10:59 +0100

    Add new test for add movie (refactor)

--- client/addNewMovie.jsx
+++ client/addNewMovie.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 
+function FormInput({ label, value, onChangeValue }) {
+  return (
+    <div>
+      <label>
+        <strong>{label}</strong>{" "}
+        <input value={value} onChange={(e) => onChangeValue(e.target.value)} />
+      </label>
+    </div>
+  );
+}
+
 export function AddNewMovie() {
   const [title, setTitle] = useState("");
   return (
     <form>
       <h1>Add new movie</h1>
 
-      <div>
-        <label>
-          <strong>Title:</strong>{" "}
-          <input value={title} onChange={(e) => setTitle(e.target.value)} />
-        </label>
-      </div>
+      <FormInput label={"Title:"} value={title} onChangeValue={setTitle} />
     </form>
   );
 }
