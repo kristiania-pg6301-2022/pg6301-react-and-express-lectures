4144f653126911ed2e57d09faaad295ccc5bff64
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-25 15:01:22 +0100

    Test for inserting new movies

--- server/__tests__/moviesApi.test.js
+++ server/__tests__/moviesApi.test.js
@@ -3,18 +3,41 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import { MoviesApi } from "../moviesApi";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
 
+const mongoClient = new MongoClient(process.env.MONGODB_URL);
 beforeAll(async () => {
-  const mongoClient = new MongoClient(process.env.MONGODB_URL);
   await mongoClient.connect();
-  app.use("/api/movies", MoviesApi(mongoClient.db("sample_mflix")));
+  const database = mongoClient.db("test_database");
+  await database.collection("movies").deleteMany({});
+  app.use("/api/movies", MoviesApi(database));
+});
+afterAll(() => {
+  mongoClient.close();
 });
 
 describe("movies api", () => {
+  it("adds a new movie", async () => {
+    await request(app)
+      .post("/api/movies")
+      .send({
+        title: "My Test Movie",
+        country: "Ukraine",
+        year: 2020,
+      })
+      .expect(200);
+    expect(
+      (await request(app).get("/api/movies").expect(200)).body.map(
+        ({ title }) => title
+      )
+    ).toContain("My Test Movie");
+  });
+
   it("lists existing movies", async () => {
     expect(
       (await request(app).get("/api/movies").expect(200)).body.map(
--- server/moviesApi.js
+++ server/moviesApi.js
@@ -29,12 +29,14 @@ export function MoviesApi(mongoDatabase) {
     res.json(movies);
   });
 
-  router.post("/new", (req, res) => {
-    const { title } = req.body;
+  router.post("/", (req, res) => {
+    const { title, country, year } = req.body;
     const result = mongoDatabase.collection("movies").insertOne({
       title,
+      countries: [country],
+      year,
     });
-    res.sendStatus(500);
+    res.sendStatus(200);
   });
 
   return router;
