72d5a9d1a1cb162d24e69752549c7a97435ed6e1
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-25 13:48:07 +0100

    Complete Setup jest testing for React

--- client/__tests__/listMovies.test.jsx
+++ client/__tests__/listMovies.test.jsx
@@ -1,3 +1,12 @@
+import { ListMovies } from "../listMovies";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
 describe("ListMovies component", () => {
-  it("shows loading screen", () => {});
+  it("shows loading screen", () => {
+    const domElement = document.createElement("div");
+    ReactDOM.render(<ListMovies />, domElement);
+    expect(domElement.innerHTML).toMatchSnapshot();
+  });
 });
