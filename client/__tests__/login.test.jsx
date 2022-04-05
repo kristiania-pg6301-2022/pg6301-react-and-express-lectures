b56d80e02fe3c9e36d0ab32c03da2b9aa0e3d25d
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-04-05 23:21:51 +0200

    Complete test for login

--- client/__tests__/login.test.jsx
+++ client/__tests__/login.test.jsx
@@ -1,17 +1,30 @@
-import { Simulate } from "react-dom/test-utils";
+import { act, Simulate } from "react-dom/test-utils";
 import ReactDOM from "react-dom";
 import React from "react";
 import { LoginPage } from "../pages/loginPage";
 
 describe("login page", () => {
-  it("logs in with google", () => {
+  it("logs in with google", async () => {
     // replace window.location to be able to detect redirects
     delete window.location;
     window.location = new URL("https://www.example.com");
 
+    global.fetch = async () => ({
+      ok: true,
+      json() {
+        return {
+          authorization_endpoint: "https://foo.example.com/auth",
+        };
+      },
+    });
+
     const domElement = document.createElement("div");
     ReactDOM.render(<LoginPage />, domElement);
-    Simulate.click(domElement.querySelector("button"));
-    expect(window.location.href).toEqual("https://foo.example.com/");
+    await act(async () => {
+      await Simulate.click(domElement.querySelector("button"));
+    });
+    expect(window.location.href).toEqual(
+      `https://foo.example.com/auth?response_type=token&client_id=1095582733852-smqnbrhcoiasjjg8q28u0g1k3nu997b0.apps.googleusercontent.com&scope=email+profile&redirect_uri=https%3A%2F%2Fwww.example.com%2Flogin%2Fcallback`
+    );
   });
 });
--- client/pages/loginPage.jsx
+++ client/pages/loginPage.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { fetchJSON } from "../lib/fetchJSON";
 
 export function LoginPage() {
-  function handleLoginWithGoogle() {
-    window.location.href = "https://foo.example.com";
+  async function handleLoginWithGoogle() {
+    const { authorization_endpoint } = await fetchJSON(
+      "https://accounts.google.com/.well-known/openid-configuration"
+    );
+
+    const parameters = {
+      response_type: "token",
+      client_id:
+        "1095582733852-smqnbrhcoiasjjg8q28u0g1k3nu997b0.apps.googleusercontent.com",
+      scope: "email profile",
+      redirect_uri: window.location.origin + "/login/callback",
+    };
+
+    window.location.href =
+      authorization_endpoint + "?" + new URLSearchParams(parameters);
   }
 
   return (
