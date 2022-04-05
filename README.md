20b788ac52d54a1106be35ef2fefff3c09854925
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-04-05 22:49:34 +0200

    Add global layout

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Making Movies Database ready for the exam
 * [x] Ensure that it's working to add a movie
 * [x] Extract stuff
 * [x] Refactor frontend to use React.Context
-* [ ] Add "country" query to List
+* [x] Add "country" query to List
 * [ ] Add login with google
   * [ ] Add test for login
 * [ ] Add login with Github
--- client/application.jsx
+++ client/application.jsx
@@ -1,17 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { FrontPage } from "./pages/frontPage";
 import { ListMovies } from "./pages/listMovies";
 import { AddNewMovie } from "./pages/addNewMovie";
 import React from "react";
 
+import "./application.css";
+
 export function Application() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<FrontPage />} />
-        <Route path={"/movies"} element={<ListMovies />} />
-        <Route path={"/movies/new"} element={<AddNewMovie />} />
-      </Routes>
+      <header>
+        <Link to={"/"}>Front page</Link>
+        <Link to={"/movies"}>Movies</Link>
+        <div className="menu-divider" />
+        <Link to={"/login"}>Login</Link>
+      </header>
+      <main>
+        <Routes>
+          <Route path={"/"} element={<FrontPage />} />
+          <Route path={"/movies"} element={<ListMovies />} />
+          <Route path={"/movies/new"} element={<AddNewMovie />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
